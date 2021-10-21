154fd5ad2239fc09ca6fe74b6c108b7991125e3a
Jose Manuel <carrizosa.josechema@gmail.com>
2021-10-21 14:35:33 -0500

    Cambio nombre de modulo de hilt.

--- app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
+++ app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
@@ -1,6 +1,4 @@
 package dev.cardoso.quotesmvvm.core
 
-//const val BASE_URL= "http://192.168.0.7"
-const val BASE_URL = "https://raw.githubusercontent.com/jomc-hash/mvc-usuarios/master/"
-//const val API_PATH="/quotes.php"
-const val API_PATH="quotes"
+const val BASE_URL= "http://192.168.0.55"
+const val API_PATH="/quotes.php"
